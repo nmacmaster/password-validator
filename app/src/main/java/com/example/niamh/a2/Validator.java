d0c57a0fa88735f4389b4eccf47a2b047d416403
nmacmaster <macmaster@dal.ca>
2018-06-08 16:09:47 -0300

    question 2 commit, v1 added blanket test and functionality for checking if password is valid built validator ui constructed ui espresso tests implemented MainActivity class for UI functionality

--- app/src/main/java/com/example/niamh/a2/Validator.java
+++ app/src/main/java/com/example/niamh/a2/Validator.java
@@ -2,14 +2,18 @@ package com.example.niamh.a2;
 
 public class Validator {
 
+    //check if password is 'password' (case insensitive)
+    //not 'password', return true, else return false
     public boolean notPassword(String pass){
-        return (pass.toLowerCase()).equals("password") ? false : true;
+        return !((pass.toLowerCase()).equals("password")) ? true : false;
     }
 
+    //if password length >= 8 return true, else return false
     public boolean validLength(String pass){
         return pass.length() >= 8 ? true : false;
     }
 
+    //check if password contains multiple cases
     public boolean multipleCases(String pass){
         /*
         if password does not equal its all lowercase nor all uppercase version,
@@ -18,11 +22,18 @@ public class Validator {
         return (!pass.equals(pass.toLowerCase())&&(!pass.equals(pass.toUpperCase())))?true:false;
     }
 
+    //check if the password contains an integer
     public boolean hasNum(String pass){
         return(pass.matches(".*\\d+.*"))?true:false;
     }
 
+    //check if the password contains a special character
     public boolean hasSpecial(String pass){
         return(pass.matches(".*[!@#$%^&*()].*"))?true:false;
+    }
+
+    //check if password is 'valid' (i.e. returns true for all above methods)
+    public boolean isValidPassword(String pass){
+        return (notPassword(pass)&&validLength(pass)&&multipleCases(pass)&&hasNum(pass)&&hasSpecial(pass))?true:false;
     }
 }
--- app/src/test/java/com/example/niamh/a2/ValidatorUnitTest.java
+++ app/src/test/java/com/example/niamh/a2/ValidatorUnitTest.java
@@ -4,15 +4,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
+public class ValidatorUnitTest {
 
+    //create new validator object
     Validator vd = new Validator();
 
+    //check if password is 'password' (case insensitive)
+    //not 'password', return true, else return false
     @Test
     public void s1_isPasswordTest(){
        assertFalse(vd.notPassword("password"));
@@ -20,6 +18,7 @@ public class ExampleUnitTest {
        assertTrue(vd.notPassword("notpassword"));
     }
 
+    //if password length >= 8 return true, else return false
     @Test
     public void s1_length8Test(){
         assertTrue(vd.validLength("abc12345"));
@@ -27,6 +26,7 @@ public class ExampleUnitTest {
         assertFalse(vd.validLength("x"));
     }
 
+    //check if password contains multiple cases
     @Test
     public void s2_casesTest(){
         assertTrue(vd.multipleCases("aBcdEF1"));
@@ -34,6 +34,7 @@ public class ExampleUnitTest {
         assertFalse(vd.multipleCases("abcdef1"));
     }
 
+    //check if the password contains an integer
     @Test
     public void s2_includesNumTest(){
         assertFalse(vd.hasNum("abcdefgh"));
@@ -42,12 +43,20 @@ public class ExampleUnitTest {
         assertTrue(vd.hasNum("07734"));
     }
 
+    //check if the password contains a special character
     @Test
     public void s2_specialCharTest(){
         assertTrue(vd.hasSpecial("abc!d"));
         assertTrue(vd.hasSpecial("$#@$%^"));
         assertFalse(vd.hasSpecial("abcdE"));
         assertTrue(vd.hasSpecial("aBcd@"));
+    }
+
+    //check if password is 'valid' (i.e. returns true for all above methods)
+    @Test
+    public void s25_isValidPasswordTest(){
+        assertTrue(vd.isValidPassword("Summer2018!"));
+        assertFalse(vd.isValidPassword("PassworD1"));
     }
 
 }
