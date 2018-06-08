fa165db030ff7c57ce8f306b967dd9d881cf1efb
nmacmaster <macmaster@dal.ca>
2018-06-07 21:13:28 -0300

    question 1 commit, v3 added tests and functionality for checking numbers, special chars, and case

--- app/src/main/java/com/example/niamh/a2/Validator.java
+++ app/src/main/java/com/example/niamh/a2/Validator.java
@@ -3,10 +3,26 @@ package com.example.niamh.a2;
 public class Validator {
 
     public boolean notPassword(String pass){
-        return pass.equals("password") ? false : true;
+        return (pass.toLowerCase()).equals("password") ? false : true;
     }
 
     public boolean validLength(String pass){
         return pass.length() >= 8 ? true : false;
+    }
+
+    public boolean multipleCases(String pass){
+        /*
+        if password does not equal its all lowercase nor all uppercase version,
+        then the string contains a mix of upper and lower cases
+        */
+        return (!pass.equals(pass.toLowerCase())&&(!pass.equals(pass.toUpperCase())))?true:false;
+    }
+
+    public boolean hasNum(String pass){
+        return(pass.matches(".*\\d+.*"))?true:false;
+    }
+
+    public boolean hasSpecial(String pass){
+        return(pass.matches(".*[!@#$%^&*()].*"))?true:false;
     }
 }
--- app/src/test/java/com/example/niamh/a2/ExampleUnitTest.java
+++ app/src/test/java/com/example/niamh/a2/ExampleUnitTest.java
@@ -14,13 +14,40 @@ public class ExampleUnitTest {
     Validator vd = new Validator();
 
     @Test
-    public void s1_isNotPassword(){
+    public void s1_isPasswordTest(){
        assertFalse(vd.notPassword("password"));
+       assertFalse(vd.notPassword("passworD"));
+       assertTrue(vd.notPassword("notpassword"));
     }
 
     @Test
-    public void s1_length8(){
+    public void s1_length8Test(){
         assertTrue(vd.validLength("abc12345"));
+        assertFalse(vd.validLength("abc123"));
+        assertFalse(vd.validLength("x"));
+    }
+
+    @Test
+    public void s2_casesTest(){
+        assertTrue(vd.multipleCases("aBcdEF1"));
+        assertTrue(vd.multipleCases("AcbDfe"));
+        assertFalse(vd.multipleCases("abcdef1"));
+    }
+
+    @Test
+    public void s2_includesNumTest(){
+        assertFalse(vd.hasNum("abcdefgh"));
+        assertTrue(vd.hasNum("Summer2018"));
+        assertTrue(vd.hasNum("a1b2c3d4ef"));
+        assertTrue(vd.hasNum("07734"));
+    }
+
+    @Test
+    public void s2_specialCharTest(){
+        assertTrue(vd.hasSpecial("abc!d"));
+        assertTrue(vd.hasSpecial("$#@$%^"));
+        assertFalse(vd.hasSpecial("abcdE"));
+        assertTrue(vd.hasSpecial("aBcd@"));
     }
 
 }
